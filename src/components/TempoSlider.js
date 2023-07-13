5316d273669b116ccf02ab32dadc77faea42cd1d
James Hughes <james.a.hughes@gmail.com>
2023-07-13 15:19:49 -0400

    update slider style

--- src/components/TempoSlider.js
+++ src/components/TempoSlider.js
@@ -3,9 +3,10 @@ import "../styles/tempoSlider.css";
 const TempoSlider = ({ tempo, tempoChange }) => {
   return (
     <div id="tempo-container">
-      <label for="tempo">Tempo:</label>
+      {/* <label for="tempo">Tempo:</label> */}
       <input
         name="tempo"
+        className="tempo-container__slider"
         type="range"
         autocomplete="off"
         min="20"
@@ -14,6 +15,9 @@ const TempoSlider = ({ tempo, tempoChange }) => {
         onChange={tempoChange}
         step="2"
       />
+      {/* <div className="slider-thumb">
+        <div className="tooltip"></div>
+      </div> */}
       <p id="tempo-value">{`${Math.round(tempo)} BPM`}</p>
     </div>
   );
